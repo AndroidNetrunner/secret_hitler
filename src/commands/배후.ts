383eff55de72b32b556ddf0b5ab9db3206b62430
AndroidNetrunner <kby9906@naver.com>
2021-10-23 15:38:32 +0900

    [FIX] 배후와 밸런스 커맨드를 시작한 게임이 없을 때 사용할 수 없도록 수정

--- src/commands/배후.ts
+++ src/commands/배후.ts
@@ -5,7 +5,11 @@ const mastermind : ICommand = {
     category: 'joining',
     description: 'toggle mastermind mode',
     callback: ({ message }) => {
-        const currentGame = active_games.get(message.channelId) as Game_room;
+        const currentGame = active_games.get(message.channelId);
+        if (!currentGame) {
+            return `시작한 게임이 존재하지 않습니다.`;
+            return;
+        }
         currentGame.mastermind = !(currentGame.mastermind)
         message.reply(`배후 직업이 ${currentGame.mastermind ? '추가' : '삭제'}되었습니다.`);
     }
--- src/commands/밸런스.ts
+++ src/commands/밸런스.ts
@@ -6,9 +6,11 @@ const balance : ICommand = {
     category: 'joining',
     description: 'toggle balance mode',
     callback: ({ message }) => {
-        const currentGame = active_games.get(message.channelId) as Game_room;
+        const currentGame = active_games.get(message.channelId);
+        if (!currentGame)
+            return `시작한 게임이 존재하지 않습니다.`;
         currentGame.balance = !(currentGame.balance)
-        message.reply(`밸런스 모드가 ${currentGame.balance ? '켜짐' : '꺼짐'}으로 설정되었습니다.`);
+        return `밸런스 모드가 ${currentGame.balance ? '켜짐' : '꺼짐'}으로 설정되었습니다.`;
     }
 }
 
