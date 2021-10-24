ccfb19d3f1910d630219aa54e3107010cb1ba2e2
AndroidNetrunner <kby9906@naver.com>
2021-10-24 11:03:30 +0900

    [FIX] 게임이 존재하지 않을 시 명령어를 사용할 수 없도록 설정

--- src/commands/배후.ts
+++ src/commands/배후.ts
@@ -6,10 +6,8 @@ const mastermind : ICommand = {
     description: 'toggle mastermind mode',
     callback: ({ message }) => {
         const currentGame = active_games.get(message.channelId);
-        if (!currentGame) {
+        if (!currentGame)
             return `시작한 게임이 존재하지 않습니다.`;
-            return;
-        }
         currentGame.mastermind = !(currentGame.mastermind)
         message.reply(`배후 직업이 ${currentGame.mastermind ? '추가' : '삭제'}되었습니다.`);
     }
