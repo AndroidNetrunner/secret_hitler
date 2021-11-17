c927f3be34453544f870446d4d35e99358356aac
AndroidNetrunner <kby9906@naver.com>
2021-11-17 17:56:22 +0900

    [FIX] 5인 미만 게임 실행 불가

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const client = new DiscordJS.Client({
     ]
 })
 
-client.login(process.env.TOKEN).then(() => client.user?.setAvatar('secret_hitler.png'));
+client.login(process.env.TOKEN).then(() => {client.user?.setAvatar('secret_hitler.png'); client.user?.setActivity('?명령어', {type: 'PLAYING'})});
 
 
 client.on('ready', () => {
